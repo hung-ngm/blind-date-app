7cd969642918f0d93b263d8ef7090f37f9dd2931
minhhung123 <prohungnguyenminh@gmail.com>
2022-07-19 01:29:30 +0700

    Add place type

--- src/modules/navigation/Navigation.tsx
+++ src/modules/navigation/Navigation.tsx
@@ -8,7 +8,7 @@ import ProfileNavigator from './components/ProfileNavigator';
 
 const Navigation = () => {
   const { user } = useStore().userStore;
-  const isProfileCompleted = true;
+  const isProfileCompleted = false;
   let Navigator: JSX.Element = <></>;
   if (user) {
     if (isProfileCompleted) {
---
+++ src/types/place.ts
@@ -0,0 +1,6 @@
+export interface Place {
+  distance: number;
+  priceMin: number;
+  priceMax: number;
+  categories: Array<string>;
+}
