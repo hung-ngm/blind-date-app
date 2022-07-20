5472efed9f9620ce56d86717643645bc5a4567ac
minhhung123 <prohungnguyenminh@gmail.com>
2022-07-20 22:24:32 +0700

    Add profile store

--- src/modules/stores/store.ts
+++ src/modules/stores/store.ts
@@ -1,11 +1,14 @@
 import { createContext, useContext } from 'react';
 import UserStore from './userStore';
+import ProfileStore from './profileStore';
 interface Store {
   userStore: UserStore;
+  profileStore: ProfileStore;
 }
 
 export const store: Store = {
   userStore: new UserStore(),
+  profileStore: new ProfileStore(),
 }
 
 export const StoreContext = createContext(store);
