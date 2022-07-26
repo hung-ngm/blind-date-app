653adf005bf60f2d8c047ddacb13d7f40aad8f98
minhhung123 <prohungnguyenminh@gmail.com>
2022-07-27 00:00:23 +1000

    Add name, age, job and chatIcon

--- src/modules/profile/full-view/ProfileFullViewScreen.tsx
+++ src/modules/profile/full-view/ProfileFullViewScreen.tsx
@@ -1,19 +1,34 @@
 import React from 'react';
-import { View, StyleSheet, Image, SafeAreaView, ScrollView } from 'react-native';
+import { 
+  View, 
+  StyleSheet, 
+  Image, 
+  SafeAreaView, 
+  ScrollView, 
+  Text,
+  TouchableWithoutFeedback
+} from 'react-native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { AppStackParamList } from '../../../types/navigation';
 import useRootNavigation from '../../navigation/hooks/useRootNavigation';
 import BackButton from '../../../common/BackButton';
+import { Entypo } from '@expo/vector-icons';
+import { mainTheme } from '../../../themes/mainTheme';
 
 type ProfileFullViewNavigationProps = NativeStackScreenProps<AppStackParamList, "ProfileFullView">
 
 const ProfileFullViewScreen = ({ route, navigation }: ProfileFullViewNavigationProps) => {
   const nav = useRootNavigation();
   const { profile }: any = route.params;
-  const { firstName, age, job, photoUrl, prompt, promptAnswer, passions } = profile;
+  const { firstName, lastName, age, job, photoUrl, prompt, promptAnswer, passions } = profile;
 
   const handleBackButtonPressed = () => {
-     nav.navigate('Home');
+    nav.navigate('Home');
+  }
+
+  const handleChatIconPressed = () => {
+    // TODO: Change this since it should navigate to the DM chat of that profile
+    nav.navigate('Chat');
   }
 
   return (
@@ -32,6 +47,23 @@ const ProfileFullViewScreen = ({ route, navigation }: ProfileFullViewNavigationP
             blurRadius={30}
           />
         </View>
+        <View style={styles.personalDetailsContainer}>
+          <View style={styles.nameAgeContainer}>
+            <Text style={styles.nameAge}>{firstName} {lastName}, {age}</Text>
+            <Text style={styles.job}>{job}</Text>
+          </View>
+          <View style={styles.chatIconContainer}>
+            <TouchableWithoutFeedback onPress={handleChatIconPressed}>
+              <Entypo name="chat" color={mainTheme.PRIMARY_COLOR} size={35} />
+            </TouchableWithoutFeedback>
+          </View>
+          
+          
+          
+          
+        </View>
+        
+        
         
       </ScrollView>
       
@@ -63,6 +95,25 @@ const styles = StyleSheet.create({
   image: {
     width: '100%',
     height: 400,
-
-  }
+  },
+  personalDetailsContainer: {
+    flexDirection: 'row',
+    paddingTop: 40,
+    paddingLeft: 30,
+  },
+  nameAgeContainer: {
+    flexDirection: 'column',
+  },
+  nameAge: {
+    fontSize: 25,
+    fontWeight: 'bold'
+  },
+  job: {
+    fontSize: 20,
+  },
+  chatIconContainer: {
+    flexDirection: 'row',
+    paddingLeft: 115,
+    paddingTop: 12,
+  },
 })
